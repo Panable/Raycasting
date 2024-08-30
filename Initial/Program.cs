544f855ad5e97f0c500270528c899bfa63fe43f6
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-08-30 12:38:21 +1000

    gitignore

--- Initial/Program.cs
+++
@@ -1,9 +0,0 @@
-namespace Initial;
-public class Program
-{
-    public static void Main()
-    {
-        Doom game = new Doom();
-        game.Run();
-    }
-}
--- Initial/lib/SplashKit.cs
+++
@@ -1,1 +0,0 @@
-/home/thepanable/.splashkit/dotnet/SplashKit.cs
